ddbad4ef6c9179b391c908a119d42137db1b7947
A1ive <10670106+a1ive@users.noreply.github.com>
2021-11-21 12:14:42 +0800

    add os version

--- sys.c
+++ sys.c
@@ -11,6 +11,83 @@ static const char* GV_GUID = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
 
 #define INFO_BUFFER_SIZE 32767
 
+static const CHAR*
+OsVersionToStr(OSVERSIONINFOEXW* p)
+{
+	if (p->dwMajorVersion == 10 && p->dwMinorVersion == 0) {
+		// FUCK YOU MICROSOFT
+		if (p->wProductType != VER_NT_WORKSTATION) {
+			if (p->dwBuildNumber >= 17763U)
+				return "Server 2019";
+			else
+				return "Server 2016";
+		}
+		else {
+			if (p->dwBuildNumber >= 22000U)
+				return "11";
+			else
+				return "10";
+		}
+	}
+	if (p->dwMajorVersion == 6 && p->dwMinorVersion == 3) {
+		if (p->wProductType != VER_NT_WORKSTATION)
+			return "Server 2012 R2";
+		else
+			return "8.1";
+	}
+	if (p->dwMajorVersion == 6 && p->dwMinorVersion == 2) {
+		if (p->wProductType != VER_NT_WORKSTATION)
+			return "Server 2012";
+		else
+			return "8";
+	}
+	if (p->dwMajorVersion == 6 && p->dwMinorVersion == 1) {
+		if (p->wProductType != VER_NT_WORKSTATION)
+			return "Server 2008 R2";
+		else
+			return "7";
+	}
+	if (p->dwMajorVersion == 6 && p->dwMinorVersion == 0) {
+		if (p->wProductType != VER_NT_WORKSTATION)
+			return "Server 2008";
+		else
+			return "Vista";
+	}
+	if (p->dwMajorVersion == 5 && p->dwMinorVersion == 2) {
+		if (p->wSuiteMask & VER_SUITE_WH_SERVER)
+			return "Home Server";
+		else if (p->wProductType == VER_NT_WORKSTATION)
+			return "XP Professional x64 Edition";
+		else
+			return "Server 2003";
+	}
+	if (p->dwMajorVersion == 5 && p->dwMinorVersion == 1) {
+		return "XP";
+	}
+	if (p->dwMajorVersion == 5 && p->dwMinorVersion == 0) {
+		return "2000";
+	}
+	return "Unknown";
+}
+
+static void PrintOsVer(void)
+{
+	NTSTATUS(WINAPI * RtlGetVersion)(LPOSVERSIONINFOEXW) = NULL;
+	OSVERSIONINFOEXW osInfo = { 0 };
+	HMODULE hMod = GetModuleHandleA("ntdll");
+
+	if (hMod)
+		*(FARPROC*)&RtlGetVersion = GetProcAddress(hMod, "RtlGetVersion");
+
+	if (RtlGetVersion)
+	{
+		osInfo.dwOSVersionInfoSize = sizeof(osInfo);
+		RtlGetVersion(&osInfo);
+		printf("OS: Windows %s (%lu.%lu.%lu)\n", OsVersionToStr(&osInfo),
+			osInfo.dwMajorVersion, osInfo.dwMinorVersion, osInfo.dwBuildNumber);
+	}
+}
+
 static void PrintOsInfo(void)
 {
 	DWORD bufCharCount = INFO_BUFFER_SIZE;
@@ -24,7 +101,7 @@ static void PrintOsInfo(void)
 		printf("Computer Name: %s\n", infoBuf);
 	bufCharCount = INFO_BUFFER_SIZE;
 	if (GetUserNameA(infoBuf, &bufCharCount))
-		printf("User name: %s\n", infoBuf);
+		printf("User Name: %s\n", infoBuf);
 	if (GetSystemDirectoryA(infoBuf, INFO_BUFFER_SIZE))
 		printf("System Directory: %s\n", infoBuf);
 	if (GetWindowsDirectoryA(infoBuf, INFO_BUFFER_SIZE))
@@ -94,7 +171,7 @@ static void PrintMemInfo(void)
 void nwinfo_sys(void)
 {
 	ObtainPrivileges(SE_SYSTEM_ENVIRONMENT_NAME);
-
+	PrintOsVer();
 	PrintOsInfo();
 	PrintFwInfo();
 	PrintMemInfo();
