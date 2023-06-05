b09fb584bec8c0d5395573c27e718d459f059c81
A1ive <10670106+a1ive@users.noreply.github.com>
2023-06-05 21:53:10 +0800

    gui change style

--- gnwinfo/gnwinfo.c
+++ gnwinfo/gnwinfo.c
@@ -55,6 +55,122 @@ get_ini_color(LPCWSTR key, struct nk_color* color)
 	free(str);
 }
 
+static struct nk_color
+convert_color(struct nk_color color, float offset)
+{
+	float h, s, v;
+	nk_color_hsv_f(&h, &s, &v, color);
+	v += offset;
+	return nk_hsv_f(h, s, v);
+}
+
+static void
+set_style(struct nk_context* ctx)
+{
+	struct nk_color text_p10 = convert_color(g_color_text_d, -0.10f);
+	struct nk_color text_p20 = convert_color(g_color_text_d, -0.20f);
+	struct nk_color text_p30 = convert_color(g_color_text_d, -0.30f);
+	struct nk_color back_p2 = convert_color(g_color_back, -0.02f);
+	struct nk_color back_p3 = convert_color(g_color_back, -0.03f);
+	struct nk_color back_p4 = convert_color(g_color_back, -0.04f);
+	struct nk_color back_n2 = convert_color(g_color_back, 0.02f);
+
+	ctx->style.text.color = g_color_text_d;
+
+	ctx->style.window.background = g_color_back;
+	ctx->style.window.fixed_background = nk_style_item_color(g_color_back);
+	ctx->style.window.min_row_height_padding = 2;
+	ctx->style.window.header.normal = nk_style_item_color(back_p2);
+	ctx->style.window.header.hover = nk_style_item_color(back_p2);
+	ctx->style.window.header.active = nk_style_item_color(back_p2);
+	ctx->style.window.header.label_normal = g_color_text_d;
+	ctx->style.window.header.label_hover = g_color_text_d;
+	ctx->style.window.header.label_active = g_color_text_d;
+	ctx->style.window.header.close_button.normal = nk_style_item_color(back_p2);
+	ctx->style.window.header.close_button.hover = nk_style_item_color(back_p2);
+	ctx->style.window.header.close_button.active = nk_style_item_color(back_p2);
+	ctx->style.window.header.close_button.text_normal = g_color_text_d;
+	ctx->style.window.header.close_button.text_hover = g_color_text_d;
+	ctx->style.window.header.close_button.text_active = g_color_text_d;
+
+	ctx->style.button.text_normal = g_color_text_d;
+	ctx->style.button.text_hover = g_color_text_d;
+	ctx->style.button.text_active = g_color_text_d;
+	ctx->style.button.normal = nk_style_item_color(back_n2);
+	ctx->style.button.hover = nk_style_item_color(back_p2);
+	ctx->style.button.active = nk_style_item_color(back_p4);
+	ctx->style.button.rounding = 0;
+	ctx->style.button.border = 1.0f;
+	ctx->style.button.padding = nk_vec2(0.0f, 0.0f);
+
+	ctx->style.checkbox.text_normal = g_color_text_d;
+	ctx->style.checkbox.text_hover = g_color_text_d;
+	ctx->style.checkbox.text_active = g_color_text_d;
+	ctx->style.checkbox.cursor_normal = nk_style_item_color(g_color_back);
+	ctx->style.checkbox.cursor_hover = nk_style_item_color(g_color_back);
+	ctx->style.checkbox.normal = nk_style_item_color(text_p30);
+	ctx->style.checkbox.hover = nk_style_item_color(text_p20);
+	ctx->style.checkbox.active = nk_style_item_color(text_p20);
+
+	ctx->style.slider.bar_normal = back_p3;
+	ctx->style.slider.bar_hover = back_p3;
+	ctx->style.slider.bar_active = back_p3;
+	ctx->style.slider.bar_filled = text_p30;
+	ctx->style.slider.cursor_normal = nk_style_item_color(text_p30);
+	ctx->style.slider.cursor_hover = nk_style_item_color(text_p20);
+	ctx->style.slider.cursor_active = nk_style_item_color(text_p10);
+
+	ctx->style.progress.normal = nk_style_item_color(back_p3);
+	ctx->style.progress.hover = nk_style_item_color(back_p3);
+	ctx->style.progress.active = nk_style_item_color(back_p3);
+
+	ctx->style.combo.normal = nk_style_item_color(g_color_back);
+	ctx->style.combo.hover = nk_style_item_color(g_color_back);
+	ctx->style.combo.active = nk_style_item_color(g_color_back);
+	ctx->style.combo.button.normal = nk_style_item_color(g_color_back);
+	ctx->style.combo.button.hover = nk_style_item_color(g_color_back);
+	ctx->style.combo.button.active = nk_style_item_color(g_color_back);
+	ctx->style.combo.button.text_normal = g_color_text_d;
+	ctx->style.combo.button.text_hover = g_color_text_d;
+	ctx->style.combo.button.text_active = g_color_text_d;
+
+	ctx->style.property.normal = nk_style_item_color(back_p3);
+	ctx->style.property.hover = nk_style_item_color(back_p3);
+	ctx->style.property.active = nk_style_item_color(back_p3);
+	ctx->style.property.label_normal = g_color_text_d;
+	ctx->style.property.label_hover = g_color_text_d;
+	ctx->style.property.label_active = g_color_text_d;
+	ctx->style.property.dec_button.normal = nk_style_item_color(back_p3);
+	ctx->style.property.dec_button.hover = nk_style_item_color(back_p3);
+	ctx->style.property.dec_button.active = nk_style_item_color(back_p3);
+	ctx->style.property.dec_button.text_normal = g_color_text_d;
+	ctx->style.property.dec_button.text_hover = g_color_text_d;
+	ctx->style.property.dec_button.text_active = g_color_text_d;
+	ctx->style.property.inc_button = ctx->style.property.dec_button;
+	ctx->style.property.edit.normal = nk_style_item_color(back_p3);
+	ctx->style.property.edit.hover = nk_style_item_color(back_p3);
+	ctx->style.property.edit.active = nk_style_item_color(back_p3);
+	ctx->style.property.edit.cursor_normal = g_color_text_d;
+	ctx->style.property.edit.cursor_hover = g_color_text_d;
+	ctx->style.property.edit.cursor_text_normal = back_p3;
+	ctx->style.property.edit.cursor_text_hover = back_p3;
+	ctx->style.property.edit.text_normal = g_color_text_d;
+	ctx->style.property.edit.text_hover = g_color_text_d;
+	ctx->style.property.edit.text_active = g_color_text_d;
+	ctx->style.property.edit.selected_normal = g_color_text_d;
+	ctx->style.property.edit.selected_hover = g_color_text_d;
+	ctx->style.property.edit.selected_text_normal = back_p3;
+	ctx->style.property.edit.selected_text_hover = back_p3;
+
+	ctx->style.scrollh.normal = nk_style_item_color(back_p2);
+	ctx->style.scrollh.hover = nk_style_item_color(back_p2);
+	ctx->style.scrollh.active = nk_style_item_color(back_p2);
+	ctx->style.scrollh.cursor_normal = nk_style_item_color(text_p30);
+	ctx->style.scrollh.cursor_hover = nk_style_item_color(text_p20);
+	ctx->style.scrollh.cursor_active = nk_style_item_color(text_p10);
+	ctx->style.scrollv = ctx->style.scrollh;
+}
+
 int APIENTRY
 wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -141,10 +257,7 @@ wWinMain(_In_ HINSTANCE hInstance,
 
 	(void)CoInitializeEx(0, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 	gnwinfo_ctx_init(hInstance, wnd, ctx, (float)g_init_width, (float)g_init_height);
-	ctx->style.button.rounding = 0;
-	ctx->style.button.border = 1.0f;
-	ctx->style.button.padding = nk_vec2(0.0f, 0.0f);
-	ctx->style.window.min_row_height_padding = 2;
+	set_style(ctx);
 
 	str = gnwinfo_get_ini_value(L"Widgets", L"HideComponents", L"0xFFFFFFFF"); // ~0U
 	g_ctx.main_flag = strtoul(str, NULL, 16);
@@ -182,8 +295,8 @@ wWinMain(_In_ HINSTANCE hInstance,
 		nk_input_end(ctx);
 
 		/* GUI */
-		ctx->style.text.color = g_color_text_d;
-		ctx->style.window.fixed_background.data.color = g_color_back;
+		if (g_ctx.gui_settings == TRUE)
+			set_style(ctx);
 		gnwinfo_draw_main_window(ctx, g_ctx.gui_width, g_ctx.gui_height);
 		gnwinfo_draw_cpuid_window(ctx, g_ctx.gui_width, g_ctx.gui_height);
 		gnwinfo_draw_about_window(ctx, g_ctx.gui_width, g_ctx.gui_height);
