ed5f3606078bad78da7fea96a72e0cfe81bdadb7
A1ive <10670106+a1ive@users.noreply.github.com>
2023-07-04 21:55:17 +0800

    gui add build time

--- gnwinfo/about.c
+++ gnwinfo/about.c
@@ -8,7 +8,7 @@ gnwinfo_draw_about_window(struct nk_context* ctx, float width, float height)
 	if (g_ctx.gui_about == FALSE)
 		return;
 	if (!nk_begin(ctx, "About",
-		nk_rect(width / 4.0f, height / 3.0f, width / 2.0f, height / 6.0f),
+		nk_rect(width / 4.0f, height / 3.0f, width / 2.0f, height / 5.0f),
 		NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_CLOSABLE))
 	{
 		g_ctx.gui_about = FALSE;
@@ -19,6 +19,7 @@ gnwinfo_draw_about_window(struct nk_context* ctx, float width, float height)
 	nk_spacer(ctx);
 	nk_label(ctx, "NWinfo GUI", NK_TEXT_CENTERED);
 	nk_label(ctx, "Copyright (c) 2023 A1ive", NK_TEXT_CENTERED);
+	nk_label(ctx, "Build. " __DATE__ " " __TIME__, NK_TEXT_CENTERED);
 
 out:
 	nk_end(ctx);
