344b3861b19326d3521d32a5b8b9db6f1781b07a
A1ive <10670106+a1ive@users.noreply.github.com>
2024-12-02 22:14:47 +0900

    add gui for ps1 script

--- hw_report.ps1
+++ hw_report.ps1
@@ -1,15 +1,24 @@
 ﻿# SPDX-License-Identifier: Unlicense
 
-#[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+# Set the working directory to the script's location
+Set-Location -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Path)
 
+# Check if the script is running with administrator privileges
+if　(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+	Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`" `"$($MyInvocation.MyCommand.UnboundArguments)`""
+	Exit
+}
+
+# Determine the appropriate nwinfo executable based on the OS architecture
 if (([Environment]::Is64BitOperatingSystem) -And (Test-Path ".\nwinfox64.exe")) {
 	$programPath = ".\nwinfox64.exe"
 } else {
 	$programPath = ".\nwinfo.exe"
 }
 
+# Define arguments to pass to the nwinfo executable
 $programArgs = @("--format=json",
-	"--cp=ansi",
+	"--cp=utf8",
 	"--human",
 	"--sys",
 	"--cpu",
@@ -21,11 +30,18 @@ $programArgs = @("--format=json",
 	"--disk=phys",
 	"--audio"
 	)
-
+# Execute nwinfo and capture its output
 $processOutput = & $programPath $programArgs
 
-$parsedJson = $processOutput | ConvertFrom-Json
+# Ensure the output is UTF-8 encoded
+# [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$utf8Bytes = [System.Text.Encoding]::Default.GetBytes($processOutput)
+$utf8Json = [System.Text.Encoding]::UTF8.GetString($utf8Bytes)
 
+# Parse the JSON output into a PowerShell object
+$parsedJson = $utf8Json | ConvertFrom-Json
+
+# Initialize a dictionary to group SMBIOS data by Table Type
 $dmiTable = @{}
 foreach ($smbiosTable in $parsedJson.'SMBIOS') {
 	$tableType = $smbiosTable.'Table Type'
@@ -37,26 +53,28 @@ foreach ($smbiosTable in $parsedJson.'SMBIOS') {
 	}
 }
 
-Write-Output "NWinfo Hardware Report - $(Get-Date -Format 'yyyyMMdd HH:mm:ss')"
-Write-Output "NWinfo $($parsedJson.'libnw') Copyright (c) 2024 A1ive"
+# Prepare the hardware report text
+$outputText = @()
+$outputText += "NWinfo Hardware Report - $(Get-Date -Format 'yyyyMMdd HH:mm:ss')"
+$outputText += "NWinfo $($parsedJson.'libnw') Copyright (c) 2024 A1ive"
 
-Write-Output "OS:"
+$outputText += "OS:"
 $systemTable = $parsedJson.'System'
-Write-Output "`t$($systemTable.'Os')"
-Write-Output "`t`t$($systemTable.'Build Number')"
-Write-Output "`t`t$($systemTable.'Edition') $($systemTable.'Processor Architecture')"
+$outputText += "`t$($systemTable.'Os')"
+$outputText += "`t`t$($systemTable.'Build Number')"
+$outputText += "`t`t$($systemTable.'Edition') $($systemTable.'Processor Architecture')"
 
-Write-Output "CPU:"
+$outputText += "CPU:"
 $cpuTable = $parsedJson.'CPUID'.'CPU0'
-Write-Output "`t$($cpuTable.'Brand')"
-Write-Output "`t`t$($cpuTable.'Code Name')"
-Write-Output "`t`t$($cpuTable.'Cores') cores $($cpuTable.'Logical CPUs') threads"
-Write-Output "`t`t$($cpuTable.'Temperature (C)')°C"
+$outputText += "`t$($cpuTable.'Brand')"
+$outputText += "`t`t$($cpuTable.'Code Name')"
+$outputText += "`t`t$($cpuTable.'Cores') cores $($cpuTable.'Logical CPUs') threads"
+$outputText += "`t`t$($cpuTable.'Temperature (C)')°C"
 if ($null -ne $dmiTable[4]) {
-	Write-Output "`t`t$($dmiTable[4][0].'Socket Designation') socket"
+	$outputText += "`t`t$($dmiTable[4][0].'Socket Designation') socket"
 }
 
-Write-Output "RAM:"
+$outputText += "RAM:"
 $maxCapacity = "unknown"
 $numSlots = "unknown"
 if ($null -ne $dmiTable[16]) {
@@ -66,49 +84,49 @@ if ($null -ne $dmiTable[16]) {
 	$maxCapacity = "$($dmiTable[5][0].'Max Memory Module Size (MB)') MB"
 	$numSlots = $dmiTable[5][0].'Number of Slots'
 }
-Write-Output "`tMax Capacity $maxCapacity $numSlots slots"
+$outputText += "`tMax Capacity $maxCapacity $numSlots slots"
 if ($null -ne $dmiTable[17]) {
 	foreach ($mdTable in $dmiTable[17]) {
-		Write-Output "`t$($mdTable.'Device Type')-$($mdTable.'Speed (MT/s)') $($mdTable.'Device Size') $($mdTable.'Manufacturer')"
-		Write-Output "`t`t$($mdTable.'Device Locator') $($mdTable.'Bank Locator') $($mdTable.'Form Factor')"
-		Write-Output "`t`t$($mdTable.'Serial Number') $($mdTable.'Part Number')"
+		$outputText += "`t$($mdTable.'Device Type')-$($mdTable.'Speed (MT/s)') $($mdTable.'Device Size') $($mdTable.'Manufacturer')"
+		$outputText += "`t`t$($mdTable.'Device Locator') $($mdTable.'Bank Locator') $($mdTable.'Form Factor')"
+		$outputText += "`t`t$($mdTable.'Serial Number') $($mdTable.'Part Number')"
 	}
 }
 
-Write-Output "Firmware:"
+$outputText += "Firmware:"
 $fwTable = $parsedJson.'UEFI'
-Write-Output "`t$($fwTable.'Firmware Type')"
-Write-Output "`tSecure Boot $($fwTable.'Secure Boot')"
+$outputText += "`t$($fwTable.'Firmware Type')"
+$outputText += "`tSecure Boot $($fwTable.'Secure Boot')"
 if ($null -ne $dmiTable[0]) {
-	Write-Output "`t$($dmiTable[0][0].'Vendor')"
-	Write-Output "`t$($dmiTable[0][0].'Version')"
-	Write-Output "`t$($dmiTable[0][0].'Release Date') $($dmiTable[0][0].'System BIOS Version')"
+	$outputText += "`t$($dmiTable[0][0].'Vendor')"
+	$outputText += "`t$($dmiTable[0][0].'Version')"
+	$outputText += "`t$($dmiTable[0][0].'Release Date') $($dmiTable[0][0].'System BIOS Version')"
 }
 
-Write-Output "Base Board:"
+$outputText += "Base Board:"
 if ($null -ne $dmiTable[2]) {
-	Write-Output "`t$($dmiTable[2][0].'Manufacturer') $($dmiTable[2][0].'Product Name') $($dmiTable[2][0].'Version')"
-	Write-Output "`t$($dmiTable[2][0].'Serial Number')"
+	$outputText += "`t$($dmiTable[2][0].'Manufacturer') $($dmiTable[2][0].'Product Name') $($dmiTable[2][0].'Version')"
+	$outputText += "`t$($dmiTable[2][0].'Serial Number')"
 }
 
-Write-Output "Graphics:"
+$outputText += "Graphics:"
 foreach ($gpuTable in $parsedJson.'GPU') {
-	Write-Output "`t$($gpuTable.'Memory Size') $($gpuTable.'Device')"
+	$outputText += "`t$($gpuTable.'Memory Size') $($gpuTable.'Device')"
 }
 foreach ($displayTable in $parsedJson.'Display') {
 	if ($null -ne $displayTable.'Manufacturer') {
-		Write-Output "`t$($displayTable.'Manufacturer') $($displayTable.'ID') ($($displayTable.'Max Resolution')@$($displayTable.'Max Refresh Rate (Hz)')) $($displayTable.'Diagonal (in)')'"
+		$outputText += "`t$($displayTable.'Manufacturer') $($displayTable.'ID') ($($displayTable.'Max Resolution')@$($displayTable.'Max Refresh Rate (Hz)')) $($displayTable.'Diagonal (in)')'"
 	}
 }
 
-Write-Output "Network:"
+$outputText += "Network:"
 foreach ($netTable in $parsedJson.'Network') {
-	Write-Output "`t$($netTable.'Description')"
+	$outputText += "`t$($netTable.'Description')"
 }
 
-Write-Output "Storage:"
+$outputText += "Storage:"
 foreach ($diskTable in $parsedJson.'Disks') {
-	Write-Output "`t$($diskTable.'HW Name')"
+	$outputText += "`t$($diskTable.'HW Name')"
 	$diskType = "HDD"
 	if ($diskTable.'Path'.StartsWith("\\.\CdRom")) {
 		$diskType = "CD-ROM"
@@ -116,19 +134,50 @@ foreach ($diskTable in $parsedJson.'Disks') {
 	elseif ($diskTable.'SSD' -eq "Yes") {
 		$diskType = "SSD"
 	}
-	Write-Output "`t`t$($diskTable.'Size') $diskType $($diskTable.'Type')"
+	$outputText += "`t`t$($diskTable.'Size') $diskType $($diskTable.'Type')"
 	if ($null -ne $diskTable.'Serial Number') {
-		Write-Output "`t`t$($diskTable.'Serial Number')"
+		$outputText += "`t`t$($diskTable.'Serial Number')"
 	}
 	if ($null -ne $diskTable.'Health Status') {
-		Write-Output "`t`t$($diskTable.'Temperature (C)')°C $($diskTable.'Health Status')"
+		$outputText += "`t`t$($diskTable.'Temperature (C)')°C $($diskTable.'Health Status')"
 	}
 }
 
-Write-Output "Audio:"
+$outputText += "Audio:"
 $audioTable = $parsedJson.'Audio'
 foreach ($i in $audioTable.PSObject.Properties.Name) {
 	if ($i -ne "Default") {
-		Write-Output "`t$($audioTable.$i.'Name')"
+		$outputText += "`t$($audioTable.$i.'Name')"
 	}
 }
+
+# Hide the powershell window: https://stackoverflow.com/a/27992426/1069307
+Add-Type -Name Window -Namespace WinAPI -MemberDefinition '
+	[DllImport("user32.dll")]
+	public static extern bool ShowWindow(int handle, int state);'
+$handle = (Get-Process -Id $PID).MainWindowHandle
+[WinAPI.Window]::ShowWindow($handle, 0)
+
+# Create a Windows Forms window to display the report
+# Write-Output $outputText
+Add-Type -AssemblyName System.Windows.Forms
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Hardware Report"
+$form.Size = New-Object System.Drawing.Size(800, 600)
+# $form.StartPosition = "CenterScreen"
+# Set the form's icon from the nwinfo executable
+$icon = [System.Drawing.Icon]::ExtractAssociatedIcon((Resolve-Path $programPath).Path)
+$form.Icon = $icon
+
+# Add a multiline, scrollable, read-only text box for the report
+$textbox = New-Object System.Windows.Forms.TextBox
+$textbox.Multiline = $true
+$textbox.ScrollBars = "Vertical"
+$textbox.ReadOnly = $true
+$textbox.Dock = "Fill"
+$textbox.Text = $outputText -join "`r`n"
+
+$form.Controls.Add($textbox)
+
+# Show the form
+$form.ShowDialog()
