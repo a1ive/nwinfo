4f9e9bdccdcc123dafb135b22eeef2c484a4ba60
A1ive <10670106+a1ive@users.noreply.github.com>
2025-04-03 19:33:01 +0900

    return dummy data if libcdi is not loaded (#68)

--- libnw/libcdi.c
+++ libnw/libcdi.c
@@ -84,11 +84,15 @@ fail:
 
 CONST CHAR* WINAPI cdi_get_version(VOID)
 {
+	if (m_cdi.cdi_get_version == NULL)
+		return "";
 	return m_cdi.cdi_get_version();
 }
 
 VOID WINAPI cdi_destroy_smart(CDI_SMART* ptr)
 {
+	if (m_cdi.cdi_destroy_smart == NULL)
+		return;
 	m_cdi.cdi_destroy_smart(ptr);
 	FreeLibrary(m_cdi.dll);
 	ZeroMemory(&m_cdi, sizeof(m_cdi));
@@ -96,65 +100,91 @@ VOID WINAPI cdi_destroy_smart(CDI_SMART* ptr)
 
 VOID WINAPI cdi_init_smart(CDI_SMART* ptr, UINT64 flags)
 {
+	if (m_cdi.cdi_init_smart == NULL)
+		return;
 	m_cdi.cdi_init_smart(ptr, flags);
 }
 
 DWORD WINAPI cdi_update_smart(CDI_SMART* ptr, INT index)
 {
+	if (m_cdi.cdi_update_smart == NULL)
+		return 0;
 	return m_cdi.cdi_update_smart(ptr, index);
 }
 
 INT WINAPI cdi_get_disk_count(CDI_SMART* ptr)
 {
+	if (m_cdi.cdi_get_disk_count == NULL)
+		return 0;
 	return m_cdi.cdi_get_disk_count(ptr);
 }
 
 BOOL WINAPI cdi_get_bool(CDI_SMART* ptr, INT index, enum CDI_ATA_BOOL attr)
 {
+	if (m_cdi.cdi_get_bool == NULL)
+		return FALSE;
 	return m_cdi.cdi_get_bool(ptr, index, attr);
 }
 
 INT WINAPI cdi_get_int(CDI_SMART* ptr, INT index, enum CDI_ATA_INT attr)
 {
+	if (m_cdi.cdi_get_int == NULL)
+		return 0;
 	return m_cdi.cdi_get_int(ptr, index, attr);
 }
 
 DWORD WINAPI cdi_get_dword(CDI_SMART* ptr, INT index, enum CDI_ATA_DWORD attr)
 {
+	if (m_cdi.cdi_get_dword == NULL)
+		return 0;
 	return m_cdi.cdi_get_dword(ptr, index, attr);
 }
 
 WCHAR* WINAPI cdi_get_string(CDI_SMART* ptr, INT index, enum CDI_ATA_STRING attr)
 {
+	if (m_cdi.cdi_get_string == NULL)
+		return NULL;
 	return m_cdi.cdi_get_string(ptr, index, attr);
 }
 
 VOID WINAPI cdi_free_string(WCHAR* ptr)
 {
+	if (m_cdi.cdi_free_string == NULL)
+		return;
 	m_cdi.cdi_free_string(ptr);
 }
 
 WCHAR* WINAPI cdi_get_smart_format(CDI_SMART* ptr, INT index)
 {
+	if (m_cdi.cdi_get_smart_format == NULL)
+		return NULL;
 	return m_cdi.cdi_get_smart_format(ptr, index);
 }
 
 BYTE WINAPI cdi_get_smart_id(CDI_SMART* ptr, INT index, INT attr)
 {
+	if (m_cdi.cdi_get_smart_id == NULL)
+		return 0;
 	return m_cdi.cdi_get_smart_id(ptr, index, attr);
 }
 
 WCHAR* WINAPI cdi_get_smart_value(CDI_SMART* ptr, INT index, INT attr, BOOL hex)
 {
+	if (m_cdi.cdi_get_smart_value == NULL)
+		return NULL;
 	return m_cdi.cdi_get_smart_value(ptr, index, attr, hex);
 }
 
 INT WINAPI cdi_get_smart_status(CDI_SMART* ptr, INT index, INT attr)
 {
+	if (m_cdi.cdi_get_smart_status == NULL)
+		return 0;
 	return m_cdi.cdi_get_smart_status(ptr, index, attr);
 }
 
 WCHAR* WINAPI cdi_get_smart_name(CDI_SMART* ptr, INT index, BYTE id)
 {
+	if (m_cdi.cdi_get_smart_name == NULL)
+		return NULL;
 	return m_cdi.cdi_get_smart_name(ptr, index, id);
 }
