826531f82fa5df404e2f0fdc17512a742447982b
A1ive <10670106+a1ive@users.noreply.github.com>
2025-10-13 13:13:32 +0800

    Print PCI driver info

--- libnw/pci.c
+++ libnw/pci.c
@@ -4,29 +4,59 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <setupapi.h>
+#include <devpkey.h>
 
 #include "libnw.h"
 #include "utils.h"
 
+static void
+PrintDriverInfo(PNODE pNode, HDEVINFO hInfo, SP_DEVINFO_DATA* spData)
+{
+	FILETIME ft = { 0 };
+	DEVPROPTYPE propType;
+	if (SetupDiGetDevicePropertyW(hInfo, spData, &DEVPKEY_Device_DriverDesc, &propType, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL, 0)
+		&& propType == DEVPROP_TYPE_STRING)
+		NWL_NodeAttrSet(pNode, "Driver", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
+	if (SetupDiGetDevicePropertyW(hInfo, spData, &DEVPKEY_Device_DriverVersion, &propType, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL, 0)
+		&& propType == DEVPROP_TYPE_STRING)
+		NWL_NodeAttrSet(pNode, "Driver Version", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
+	if (SetupDiGetDevicePropertyW(hInfo, spData, &DEVPKEY_Device_DriverProvider, &propType, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL, 0)
+		&& propType == DEVPROP_TYPE_STRING)
+		NWL_NodeAttrSet(pNode, "Driver Provider", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
+	if (SetupDiGetDevicePropertyW(hInfo, spData, &DEVPKEY_Device_DriverDate, &propType, (PBYTE)&ft, sizeof(FILETIME), NULL, 0)
+		&& propType == DEVPROP_TYPE_FILETIME)
+	{
+		SYSTEMTIME st;
+		if (FileTimeToSystemTime(&ft, &st))
+			NWL_NodeAttrSetf(pNode, "Driver Date", 0, "%u-%02u-%02u", st.wYear, st.wMonth, st.wDay);
+	}
+	if (SetupDiGetDevicePropertyW(hInfo, spData, &DEVPKEY_Device_DriverInfPath, &propType, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL, 0)
+		&& propType == DEVPROP_TYPE_STRING)
+		NWL_NodeAttrSet(pNode, "Inf Path", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
+	if (SetupDiGetDevicePropertyW(hInfo, spData, &DEVPKEY_Device_DriverInfSection, &propType, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL, 0)
+		&& propType == DEVPROP_TYPE_STRING)
+		NWL_NodeAttrSet(pNode, "Inf Section", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
+}
+
 PNODE NWL_EnumPci(PNODE pNode, LPCSTR pciClass)
 {
-	HDEVINFO Info = NULL;
+	HDEVINFO hInfo = NULL;
 	DWORD i = 0;
-	SP_DEVINFO_DATA DeviceInfoData = { .cbSize = sizeof(SP_DEVINFO_DATA) };
-	DWORD Flags = DIGCF_PRESENT | DIGCF_ALLCLASSES;
-	DWORD IdsSize = 0;
-	CHAR* Ids = NWL_LoadIdsToMemory(L"pci.ids", &IdsSize);
-	Info = SetupDiGetClassDevsW(NULL, L"PCI", NULL, Flags);
-	if (Info == INVALID_HANDLE_VALUE)
+	SP_DEVINFO_DATA spData = { .cbSize = sizeof(SP_DEVINFO_DATA) };
+	DWORD dwFlags = DIGCF_PRESENT | DIGCF_ALLCLASSES;
+	DWORD idsSize = 0;
+	CHAR* ids = NWL_LoadIdsToMemory(L"pci.ids", &idsSize);
+	hInfo = SetupDiGetClassDevsW(NULL, L"PCI", NULL, dwFlags);
+	if (hInfo == INVALID_HANDLE_VALUE)
 	{
 		NWL_NodeAppendMultiSz(&NWLC->ErrLog, "SetupDiGetClassDevs failed");
 		goto fail;
 	}
-	for (i = 0; SetupDiEnumDeviceInfo(Info, i, &DeviceInfoData); i++)
+	for (i = 0; SetupDiEnumDeviceInfo(hInfo, i, &spData); i++)
 	{
-		CHAR HwClass[7] = { 0 };
+		CHAR hwClass[7] = { 0 };
 		PNODE npci;
-		if (!SetupDiGetDeviceRegistryPropertyW(Info, &DeviceInfoData,
+		if (!SetupDiGetDeviceRegistryPropertyW(hInfo, &spData,
 			SPDRP_HARDWAREID, NULL, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL))
 			continue;
 		for (LPCWSTR p = NWLC->NwBufW; p[0]; p += wcslen(p) + 1)
@@ -35,7 +65,7 @@ PNODE NWL_EnumPci(PNODE pNode, LPCSTR pciClass)
 			LPCWSTR s = wcsstr(p, L"&CC_");
 			if (s != NULL)
 			{
-				snprintf(HwClass, 7, "%ls", s + 4);
+				snprintf(hwClass, 7, "%ls", s + 4);
 				break;
 			}
 		}
@@ -44,32 +74,34 @@ PNODE NWL_EnumPci(PNODE pNode, LPCSTR pciClass)
 			size_t PciClassLen = strlen(pciClass);
 			if (PciClassLen > 6)
 				PciClassLen = 6;
-			if (_strnicmp(pciClass, HwClass, PciClassLen) != 0)
+			if (_strnicmp(pciClass, hwClass, PciClassLen) != 0)
 				continue;
 		}
 		npci = NWL_NodeAppendNew(pNode, "Device", NFLG_TABLE_ROW);
 
 		NWL_NodeAttrSet(npci, "HWID", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
-		NWL_ParseHwid(npci, Ids, IdsSize, NWLC->NwBufW, 0);
+		NWL_ParseHwid(npci, ids, idsSize, NWLC->NwBufW, 0);
 
-		if (SetupDiGetDeviceRegistryPropertyW(Info, &DeviceInfoData,
+		if (SetupDiGetDeviceRegistryPropertyW(hInfo, &spData,
 			SPDRP_DEVICEDESC, NULL, (PBYTE)NWLC->NwBufW, NWINFO_BUFSZB, NULL))
 			NWL_NodeAttrSet(npci, "Description", NWL_Ucs2ToUtf8(NWLC->NwBufW), 0);
 
-		ULONG BusNum = 0, DevFunc = 0;
-		SetupDiGetDeviceRegistryPropertyW(Info, &DeviceInfoData,
-			SPDRP_BUSNUMBER, NULL, (PBYTE)&BusNum, sizeof(BusNum), NULL);
-		SetupDiGetDeviceRegistryPropertyW(Info, &DeviceInfoData,
-			SPDRP_ADDRESS, NULL, (PBYTE)&DevFunc, sizeof(DevFunc), NULL);
-		NWL_NodeAttrSetf(npci, "Location", NAFLG_FMT_NEED_QUOTE, "Bus %u, Device %u, Function %u",
-			BusNum & 0xFF, DevFunc >> 16, DevFunc & 0xFFFF);
+		PrintDriverInfo(npci, hInfo, &spData);
 
-		NWL_NodeAttrSet(npci, "Class Code", HwClass, 0);
-		NWL_FindClass(npci, Ids, IdsSize, HwClass, 0);
+		ULONG busNum = 0, devFunc = 0;
+		SetupDiGetDeviceRegistryPropertyW(hInfo, &spData,
+			SPDRP_BUSNUMBER, NULL, (PBYTE)&busNum, sizeof(busNum), NULL);
+		SetupDiGetDeviceRegistryPropertyW(hInfo, &spData,
+			SPDRP_ADDRESS, NULL, (PBYTE)&devFunc, sizeof(devFunc), NULL);
+		NWL_NodeAttrSetf(npci, "Location", NAFLG_FMT_NEED_QUOTE, "Bus %u, Device %u, Function %u",
+			busNum & 0xFF, devFunc >> 16, devFunc & 0xFFFF);
+
+		NWL_NodeAttrSet(npci, "Class Code", hwClass, 0);
+		NWL_FindClass(npci, ids, idsSize, hwClass, 0);
 	}
-	SetupDiDestroyDeviceInfoList(Info);
+	SetupDiDestroyDeviceInfoList(hInfo);
 fail:
-	free(Ids);
+	free(ids);
 	return pNode;
 }
 
