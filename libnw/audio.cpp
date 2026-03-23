d61ca01ea2c64f15b76ae8bd2a3d282f04d4e2f9
A1ive <10670106+a1ive@users.noreply.github.com>
2026-03-23 09:53:04 +0800

    Add HDA codec info (#105)

--- libnw/audio.cpp
+++ libnw/audio.cpp
@@ -2,9 +2,121 @@
 
 #include "libnw.h"
 #include "audio.h"
+#include "devtree.h"
 #include <mmdeviceapi.h>
 #include <endpointvolume.h>
+#include <stdlib.h>
 #include <functiondiscoverykeys_devpkey.h>
+
+#define HDA_IDS_IMPL
+#include <hda_ids.h>
+
+extern "C" LPCSTR NWL_Ucs2ToUtf8(LPCWSTR src);
+
+typedef struct _AUDIO_CODEC_ENUM_CTX
+{
+	CHAR CodecHwid[DEVTREE_MAX_STR_LEN];
+} AUDIO_CODEC_ENUM_CTX;
+
+static void
+AudioSetCodecName(PNODE node, LPCSTR hwid)
+{
+	uint16_t vid = 0;
+	uint16_t did = 0;
+
+	if (strlen(hwid) < 33)
+		return;
+
+	vid = (uint16_t)strtoul(hwid + 20, NULL, 16);
+	did = (uint16_t)strtoul(hwid + 29, NULL, 16);
+
+	NWL_NodeAttrSet(node, "Codec Vendor", "Unknown", 0);
+	for (size_t i = 0; i < sizeof(HDA_VENDOR_IDS) / sizeof(HDA_VENDOR_IDS[0]); i++)
+	{
+		if (HDA_VENDOR_IDS[i].vid == vid)
+		{
+			NWL_NodeAttrSet(node, "Codec Vendor", HDA_VENDOR_IDS[i].name, 0);
+			break;
+		}
+	}
+
+	NWL_NodeAttrSet(node, "Codec Device", "HDMI/DP", 0);
+	for (size_t i = 0; i < sizeof(HDA_DEVICE_IDS) / sizeof(HDA_DEVICE_IDS[0]); i++)
+	{
+		if (HDA_DEVICE_IDS[i].vid == vid && HDA_DEVICE_IDS[i].did == did)
+		{
+			NWL_NodeAttrSet(node, "Codec Device", HDA_DEVICE_IDS[i].name, 0);
+			break;
+		}
+	}
+}
+
+static void CALLBACK
+GetDeviceInfoAudioCodec(PNODE node, void* data, DEVINST devInst, DEVINST parentDevInst, LPCSTR hwIds)
+{
+	AUDIO_CODEC_ENUM_CTX* ctx = (AUDIO_CODEC_ENUM_CTX*)data;
+	(void)node;
+	(void)devInst;
+	(void)parentDevInst;
+
+	if (ctx->CodecHwid[0] != '\0')
+		return;
+
+	strncpy_s(ctx->CodecHwid, DEVTREE_MAX_STR_LEN, hwIds, _TRUNCATE);
+}
+
+static void CALLBACK
+GetDeviceInfoAudioController(PNODE node, void* data, DEVINST devInst, DEVINST parentDevInst, LPCSTR hwIds)
+{
+	static const CHAR prefix[] = "HDAUDIO\\FUNC_01&VEN_";
+	AUDIO_CODEC_ENUM_CTX* ctx = (AUDIO_CODEC_ENUM_CTX*)data;
+	DEVTREE_ENUM_CTX codecCtx = { 0 };
+	PNODE devTree;
+	(void)node;
+	(void)parentDevInst;
+
+	(void)hwIds;
+
+	if (ctx->CodecHwid[0] != '\0')
+		return;
+
+	strncpy_s(codecCtx.filter, DEVTREE_MAX_STR_LEN, prefix, _TRUNCATE);
+	codecCtx.filterLen = sizeof(prefix) - 1;
+	codecCtx.hub = "Devices";
+	codecCtx.data = ctx;
+	codecCtx.GetDeviceInfo = GetDeviceInfoAudioCodec;
+
+	devTree = NWL_NodeAlloc("Device Tree", NFLG_TABLE);
+	NWL_EnumerateDevices(devTree, &codecCtx, devInst, 0);
+	NWL_NodeFree(devTree, 1);
+}
+
+static void
+AudioSetCodecHwid(PNODE node, LPCSTR hwid, DEVINST devRoot)
+{
+	DEVTREE_ENUM_CTX ctx = { 0 };
+	AUDIO_CODEC_ENUM_CTX data = { 0 };
+	PNODE devTree;
+
+	if (devRoot == 0)
+		return;
+
+	strncpy_s(ctx.filter, DEVTREE_MAX_STR_LEN, hwid, _TRUNCATE);
+	ctx.filterLen = strlen(ctx.filter);
+	ctx.hub = "Devices";
+	ctx.data = &data;
+	ctx.GetDeviceInfo = GetDeviceInfoAudioController;
+
+	devTree = NWL_NodeAlloc("Device Tree", NFLG_TABLE);
+	NWL_EnumerateDevices(devTree, &ctx, devRoot, 0);
+	NWL_NodeFree(devTree, 1);
+
+	if (data.CodecHwid[0] != '\0')
+	{
+		NWL_NodeAttrSet(node, "Codec HWID", data.CodecHwid, 0);
+		AudioSetCodecName(node, data.CodecHwid);
+	}
+}
 
 static LPWSTR AudioGetDefaultId(IMMDeviceEnumerator* p)
 {
@@ -110,30 +222,66 @@ fail:
 	return dev;
 }
 
-extern "C" LPCSTR NWL_Ucs2ToUtf8(LPCWSTR src);
-
-PNODE NW_Audio(BOOL bAppend)
+inline void PrintMMDevices(PNODE node)
 {
 	UINT count, i;
-	NWLIB_AUDIO_DEV* dev = NULL;
-	PNODE pNode = NWL_NodeAlloc("Audio", 0);
-	if (bAppend)
-		NWL_NodeAppendChild(NWLC->NwRoot, pNode);
-	dev = NWL_GetAudio(&count);
+	NWLIB_AUDIO_DEV* dev = NWL_GetAudio(&count);
 	if (!dev)
-		return pNode;
+		return;
 	for (i = 0; i < count; i++)
 	{
 		CHAR buf[32];
 		PNODE pChild;
 		snprintf(buf, 32, "%u", i);
-		pChild = NWL_NodeAppendNew(pNode, buf, NFLG_ATTGROUP);
+		pChild = NWL_NodeAppendNew(node, buf, NFLG_ATTGROUP);
 		NWL_NodeAttrSet(pChild, "Name", NWL_Ucs2ToUtf8(dev[i].name), 0);
 		NWL_NodeAttrSet(pChild, "ID", NWL_Ucs2ToUtf8(dev[i].id), 0);
 		NWL_NodeAttrSetf(pChild, "Volume", 0, "%.0f%%", 100.0f * dev[i].volume);
 		if (dev[i].is_default)
-			NWL_NodeAttrSetf(pNode, "Default", NAFLG_FMT_NUMERIC, "%u", i);
+			NWL_NodeAttrSetf(node, "Default", NAFLG_FMT_NUMERIC, "%u", i);
 	}
 	free(dev);
+}
+
+inline void PrintSoundCards(PNODE node)
+{
+	PNODE sdc = NWL_NodeAppendNew(node, "Sound Cards", NFLG_TABLE);
+	DEVINST devRoot = 0;
+
+	PNWL_ARG_SET pciClasses = NULL;
+	NWL_ArgSetAddStr(&pciClasses, "0401"); // Multimedia audio controller
+	NWL_ArgSetAddStr(&pciClasses, "0403"); // Audio device
+	PNODE pci = NWL_EnumPci(NWL_NodeAlloc("PCI", NFLG_TABLE), pciClasses);
+	NWL_ArgSetFree(pciClasses);
+
+	if (CM_Locate_DevNodeW(&devRoot, NULL, CM_LOCATE_DEVNODE_NORMAL) != CR_SUCCESS)
+		NWL_NodeAppendMultiSz(&NWLC->ErrLog, "CM_Locate_DevNodeW failed");
+
+	INT count = NWL_NodeChildCount(pci);
+	for (INT i = 0; i < count; i++)
+	{
+		PNODE dev = NWL_NodeEnumChild(pci, i);
+		if (dev == NULL)
+			continue;
+		PNODE t = NWL_NodeAppendNew(sdc, "Device", NFLG_TABLE_ROW);
+		const char* name = NWL_NodeAttrGet(dev, "Device");
+		if (name[0] == '-' && name[1] == '\0')
+			name = "High Definition Audio Controller";
+		LPCSTR hwid = NWL_NodeAttrGet(dev, "HWID");
+		NWL_NodeAttrSet(t, "HWID", hwid, 0);
+		NWL_NodeAttrSet(t, "Vendor", NWL_NodeAttrGet(dev, "Vendor"), 0);
+		NWL_NodeAttrSet(t, "Device", name, 0);
+		AudioSetCodecHwid(t, hwid, devRoot);
+	}
+	NWL_NodeFree(pci, 1);
+}
+
+PNODE NW_Audio(BOOL bAppend)
+{
+	PNODE pNode = NWL_NodeAlloc("Audio", 0);
+	if (bAppend)
+		NWL_NodeAppendChild(NWLC->NwRoot, pNode);
+	PrintMMDevices(pNode);
+	PrintSoundCards(pNode);
 	return pNode;
 }
