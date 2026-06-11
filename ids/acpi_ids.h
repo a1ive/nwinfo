8c4e6dcaf6961fe8ac1075005b62fd57eaa50c87
A1ive <10670106+a1ive@users.noreply.github.com>
2026-06-11 11:09:55 +0900

    Add ACPI vendor IDs

---
+++ ids/acpi_ids.h
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: Unlicense
+#pragma once
+
+#define ACPI_IDS_DEFAULT \
+	"AAVA\tAava Mobile\n" \
+	"AMDI\tAMD\n" \
+	"APMC\tAppliedMicro\n" \
+	"APTA\tAptina\n" \
+	"ARMH\tARM\n" \
+	"ARML\tARM\n" \
+	"ASUS\tASUS\n" \
+	"ATML\tAtmel\n" \
+	"AUTH\tAuthenTec\n" \
+	"BRCM\tBroadcom\n" \
+	"CPLM\tCapella\n" \
+	"DLLK\tDell\n" \
+	"DELL\tDell\n" \
+	"ELAN\tELAN\n" \
+	"ESSX\tEverest\n" \
+	"FTSC\tFocalTech\n" \
+	"FRSC\tFreescale\n" \
+	"RKCP\tRockchip\n" \
+	"GOOG\tGoogle\n" \
+	"HPQC\tHP\n" \
+	"HWPE\tHPE\n" \
+	"HIMX\tHimax\n" \
+	"HISI\tHiSilicon\n" \
+	"HPIC\tHP\n" \
+	"HTLM\tHTBLuVA Mödling\n" \
+	"IBMX\tIBM\n" \
+	"IMPJ\tImpinj\n" \
+	"IPHI\tInphi\n" \
+	"ACPI\tIntel\n" \
+	"INTC\tIntel\n" \
+	"INTL\tIntel\n" \
+	"INVN\tInvensense\n" \
+	"IP3T\tIP3\n" \
+	"KIOX\tKionix\n" \
+	"IDEA\tLenovo\n" \
+	"LNRO\tLinaro\n" \
+	"MSAY\tMicrosoft\n" \
+	"MSFT\tMicrosoft\n" \
+	"MSHW\tMicrosoft\n" \
+	"MIPI\tMIPI Alliance\n" \
+	"NVTN\tNuvoton\n" \
+	"NVDA\tNvidia\n" \
+	"OVTI\tOmniVision\n" \
+	"PEGA\tPegatron\n" \
+	"QCOM\tQualcomm\n" \
+	"OBDA\tREALTEK\n" \
+	"QEMU\tRed Hat\n" \
+	"BOSC\tBosch\n" \
+	"RZSN\tRozsnyó, s.r.o.\n" \
+	"SHRP\tSharp\n" \
+	"DSUO\tDouble Star\n" \
+	"ST86\tSouth-Top\n" \
+	"3NOD\t3Nod\n" \
+	"SWEM\tSierra Wireless\n" \
+	"SONY\tSony\n" \
+	"SYNA\tSynaptics\n" \
+	"TCAG\tTeracue\n" \
+	"TXNW\tTexas Instruments\n" \
+	"LNUX\tLinux\n" \
+	"TOSB\tDynabook\n" \
+	"VAIO\tVAIO\n" \
+	"VFSI\tValidity Sensors\n" \
+	"WCOM\tWacom\n" \
+	"WSDR\tWinsider\n" \
+	"MXIM\tMaxim Integrated\n" \
+	"XMCC\tXiaomi\n" \
+	"UBLX\tu-blox\n" \
+	"RAYD\tRaydium\n" \
+	"DLGS\tDialog-Semi\n" \
+	"OMPS\tOmniPreSense\n" \
+	"CORE\tCoreOS\n" \
+	"MCHP\tMicrochip\n" \
+	"GHSW\tGHS\n" \
+	"AANT\tAAEON\n" \
+	"3GVR\tVR\n" \
+	"EXAR\tExar\n" \
+	"BOOT\tCoreboot\n" \
+	"MRVL\tMarvell\n" \
+	"IHSE\tIHSE\n" \
+	"INSY\tInsyde\n" \
+	"NXGO\tNexstgo\n" \
+	"AMPC\tAmpere\n" \
+	"IDEM\tIDEMIA\n" \
+	"VSHY\tVishay\n" \
+	"DMST\tDMIST\n" \
+	"CMHR\tCOMHEAR\n" \
+	"SNSL\tSensel\n" \
+	"GTCH\tG2touch\n" \
+	"HXTS\tHXT\n" \
+	"AMZN\tAmazon\n" \
+	"ASEM\tASEM\n" \
+	"FUJI\tFujitsu\n" \
+	"PHYT\tPhytium\n" \
+	"HYGO\tHygon\n" \
+	"PIXA\tPixArt\n" \
+	"LOON\tLoongson\n" \
+	"SECC\tEpson\n" \
+	"BABA\tAlibaba\n" \
+	"JSYS\tJuniper\n" \
+	"FRMW\tFramework\n" \
+	"PNSO\tPensando\n" \
+	"DNBK\tDynabook\n" \
+	"DIOO\tDioo\n" \
+	"PURI\tPurism\n" \
+	"LTSC\tLontium\n" \
+	"WACF\tWacom\n" \
+	"AWDZ\tAwinic\n" \
+	"SILC\tSilicom\n" \
+	"NOLO\tNOLO\n" \
+	"GOUP\tGoUp\n" \
+	"JMIC\tJaguarMicro\n" \
+	"ELAS\tElliptic Labs\n" \
+	"MCRY\tMicro Crystal\n" \
+	"CIXH\tCixin Tech\n" \
+	"ETDS\tEyeTech\n" \
+	"RPIL\tRaspberry Pi\n" \
+	"ISIC\tIstaric\n" \
+	"CIRQ\tCirque\n" \
+	"RVOS\tRivos\n" \
+	"BECS\tBeijer\n" \
+	"ILIT\tILI\n" \
+	"HJMC\tHJ-Micro\n" \
+	"NAIM\tVervent Audio\n" \
+	"UBOO\tDas U-Boot\n" \
+	"DSHR\t3mdeb\n" \
+	"SGSN\tSigmaSense\n" \
+	"INIT\tINIT\n" \
+	"RSCV\tRISC-V\n" \
+	"VNTN\tVentana\n" \
+	"DBSI\tDreamBig\n" \
+	"SUNW\tWXUT\n" \
+	"SOPH\tSophgo\n" \
+	"SSLC\tSmartSens\n" \
+	"AREU\tAreus\n" \
+	"ROKL\tRockwell\n" \
+	"JUMP\tJUMPtec\n" \
+	"FSAS\tFsasTech\n" \
+	"JPMC\tJPMorgan Chase\n" \
+	"ROKU\tRoku\n" \
+	"ULRV\tUltraRISC\n" \
+	"SYNC\tSYNCS / AvIoT\n" \
+	"AHCL\tAdvantech\n" \
+	"PICO\tPicoheart.\n" \
+	"KOMF\tKontron\n" \
+	"UBTI\tUbiquiti\n" \
+	"KAYA\tKAYA\n" \
+	"MITH\tMesiontech\n" \
+	"NXHP\tNexthop\n" \
+	"MPTR\tMegapolis-Telecom\n" \
+	"TYHX\tTianyi Hexin\n" \
+	"LECA\tTheo End\n" \
+	"SIPL\tSIPEARL\n" \
+	"GHIP\tGreen Hippo\n" \
+	"\n" \
